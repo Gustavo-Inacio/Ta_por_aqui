316643222cb4dd87ccdb1d3f73363f0808c40d41
Gustavo Inacio <gutiinacio@gmai.com>
2021-04-26 09:47:17 -0300

    Home - Landing Section concluida e totalmente responsiva

---
+++ public/Home/home.js
@@ -0,0 +1,36 @@
+/* -- Suggestion Bar Comeco--*/
+
+let suggestionList = document.getElementsByClassName("my-services-suggestion-bar")[0];
+let suggestionController = { // estas sao as setas laterais da lista
+    left: document.getElementById("mySuggestionBarControllerLeft"),
+    right: document.getElementById("mySuggestionBarControllerRight")
+}
+
+let services = document.getElementsByClassName("my-service-item");
+
+let scroll = 0
+
+const setSuggestionBarScroll = () => { // aqui a variavel 'scroll' que controla o movimento eh aplicada a lista
+    suggestionList.style.marginLeft = scroll + "px";  
+}
+
+suggestionController.left.onclick = () => { // ao clicar para a esquerda a lista move para a direita
+    scroll += Math.floor(window.innerWidth / 2);
+    if(scroll > 0) scroll = 0; // se o usuario clicar para a esquerda, e a lista já estiver no fim, ele reestabelece para a posicao 0;
+    setSuggestionBarScroll();
+}
+
+suggestionController.right.onclick = () => { // ao clicar na seta para a direita a lista move para a esquerda
+    scroll -= Math.floor(window.innerWidth / 2);
+
+    let end = services[services.length - 1].getBoundingClientRect().right + services[services.length - 1].getBoundingClientRect().width - window.innerWidth / 2; // esse sera a distancia da esqueda da tela ate o fim da lista
+
+    if(end < window.innerWidth){ // se o usuario clicar na seta para a direita mesmo que ja estiver no final, ele mantem a sua posicao 
+        scroll += window.innerWidth - end;
+    }
+
+    setSuggestionBarScroll();
+
+}
+
+/* -- Suggestion Bar Fim--*/
