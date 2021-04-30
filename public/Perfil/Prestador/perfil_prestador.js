aceb9f9314c772efc8182af490a27cb7075f0912
Natan Barbosa <nerenatan@gmail.com>
2021-04-30 11:12:24 -0300

    Seção de serviços solicitados e serviços disponibilizados finalizada

--- public/Perfil/Prestador/perfil_prestador.js
+++ public/Perfil/Prestador/perfil_prestador.js
@@ -8,11 +8,29 @@ function changeButtonColor(){
     cancelButton.className = 'cancelEdit'
     cancelButton.disabled = ''
 
-    document.getElementById("userName").removeAttribute('readonly')
-    document.getElementById("userLastName").removeAttribute('readonly')
-    document.getElementById("userCell").removeAttribute('readonly')
-    document.getElementById("userEmail").removeAttribute('readonly')
-    document.getElementById("userSite").removeAttribute('readonly')
-    document.getElementById("userDescription").removeAttribute('readonly')
+    let name = document.getElementById("userName")
+    let lastName = document.getElementById("userLastName")
+    let cell = document.getElementById("userCell")
+    let email = document.getElementById("userEmail")
+    let site = document.getElementById("userSite")
+    let description = document.getElementById("userDescription")
 
+    name.removeAttribute('readonly')
+    name.style.color = 'black'
+    name.focus()
+
+    lastName.removeAttribute('readonly')
+    lastName.style.color = 'black'
+
+    cell.removeAttribute('readonly')
+    cell.style.color = 'black'
+
+    email.removeAttribute('readonly')
+    email.style.color = 'black'
+
+    site.removeAttribute('readonly')
+    site.style.color = 'black'
+
+    description.removeAttribute('readonly')
+    description.style.color = 'black'
 }
