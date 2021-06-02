7c7e5d78a912c82618924f2a03230e5090a8645f
Natan Barbosa <nerenatan@gmail.com>
2021-06-02 10:48:55 -0300

    Banco de dados totalmente estruturado e exportado

--- ta_por_aqui.sql
+++ ta_por_aqui.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 30-Maio-2021 às 19:22
+-- Tempo de geração: 02-Jun-2021 às 10:46
 -- Versão do servidor: 10.4.11-MariaDB
 -- versão do PHP: 7.4.4
 
@@ -20,6 +20,147 @@ SET time_zone = "+00:00";
 --
 -- Banco de dados: `ta_por_aqui`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `comentarios`
+--
+
+CREATE TABLE `comentarios` (
+  `id_comentario` int(11) NOT NULL,
+  `id_servico` int(11) NOT NULL,
+  `nota` float(3,1) NOT NULL,
+  `comentario` text DEFAULT NULL,
+  `data` date DEFAULT curdate()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `contratos`
+--
+
+CREATE TABLE `contratos` (
+  `id_contrato` int(11) NOT NULL,
+  `id_servico` int(11) NOT NULL,
+  `id_cliente` int(11) NOT NULL,
+  `id_prestador` int(11) NOT NULL,
+  `data_contrato` date DEFAULT curdate(),
+  `status_contrato` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `denuncia_comentario`
+--
+
+CREATE TABLE `denuncia_comentario` (
+  `id_denuncia_comentario` int(11) NOT NULL,
+  `id_comentario` int(11) NOT NULL,
+  `motivo_denuncia` varchar(20) NOT NULL,
+  `comentario_denuncia` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `denuncia_motivo`
+--
+
+CREATE TABLE `denuncia_motivo` (
+  `id_denuncia_motivo` int(11) NOT NULL,
+  `motivo_denuncia` varchar(20) NOT NULL,
+  `categoria` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `denuncia_servico`
+--
+
+CREATE TABLE `denuncia_servico` (
+  `id_denuncia_servico` int(11) NOT NULL,
+  `id_servico` int(11) NOT NULL,
+  `motivo_denuncia` int(11) NOT NULL,
+  `comentario_denuncia` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `fale_conosco`
+--
+
+CREATE TABLE `fale_conosco` (
+  `id_contato` int(11) NOT NULL,
+  `nome` varchar(30) NOT NULL,
+  `email` varchar(40) NOT NULL,
+  `empresa` varchar(30) DEFAULT NULL,
+  `telefone` varchar(20) NOT NULL,
+  `mensagem` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `imagens`
+--
+
+CREATE TABLE `imagens` (
+  `id_imagem` int(11) NOT NULL,
+  `id_servico` int(11) NOT NULL,
+  `dir_imagem` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `redes_sociais`
+--
+
+CREATE TABLE `redes_sociais` (
+  `id_rede_social` int(11) NOT NULL,
+  `id_usuario` int(11) NOT NULL,
+  `instragram` varchar(30) DEFAULT NULL,
+  `twitter` varchar(30) DEFAULT NULL,
+  `facebook` varchar(30) DEFAULT NULL,
+  `linkedin` varchar(30) DEFAULT NULL,
+  `tiktok` varchar(30) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `servico`
+--
+
+CREATE TABLE `servico` (
+  `id_servico` int(11) NOT NULL,
+  `prestador` int(11) NOT NULL,
+  `nome_servico` varchar(30) DEFAULT NULL,
+  `tipo` int(11) NOT NULL,
+  `categoria` text NOT NULL,
+  `descricao` text NOT NULL,
+  `preco` float(5,2) DEFAULT NULL,
+  `data_publicacao` date DEFAULT curdate(),
+  `nota_media` float(3,1) DEFAULT NULL,
+  `status_servico` int(11) DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `servicos_salvos`
+--
+
+CREATE TABLE `servicos_salvos` (
+  `id_servico_salvo` int(11) NOT NULL,
+  `id_servico` int(11) NOT NULL,
+  `id_usuario` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -52,16 +193,80 @@ CREATE TABLE `usuarios` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Extraindo dados da tabela `usuarios`
---
-
-INSERT INTO `usuarios` (`id_usuario`, `nome`, `sobrenome`, `telefone`, `email`, `senha`, `data_nascimento`, `sexo`, `classificacao`, `cep`, `estado`, `cidade`, `bairro`, `rua`, `numero`, `complemento`, `data_entrada`, `descricao`, `site`, `status_usuario`, `imagem_perfil`) VALUES
-(1, 'Natan', 'Barbosa', '(11) 54788-5223', 'natanbarbosa525@gmail.com', '123', '2003-07-28', 'M', 1, '64017808', 'PI', 'Teresina', 'Redenção', 'Rua Jorge Mazuad', '54', NULL, '2021-05-27', NULL, NULL, 0, 'no_picture.jpg'),
-(4, 'Andressa', 'Silva', '(65) 46541-3216', 'lauringamesbr@gmail.com', 'senha', '1998-05-14', 'F', 0, '05570140', 'SP', 'São Paulo', 'Jardim Amaralina', 'Rua Fausto Feijó', '78', NULL, '2021-05-30', NULL, NULL, 0, 'no_picture.jpg');
-
---
 -- Índices para tabelas despejadas
 --
+
+--
+-- Índices para tabela `comentarios`
+--
+ALTER TABLE `comentarios`
+  ADD PRIMARY KEY (`id_comentario`),
+  ADD KEY `fk_ComentarioServico` (`id_servico`);
+
+--
+-- Índices para tabela `contratos`
+--
+ALTER TABLE `contratos`
+  ADD PRIMARY KEY (`id_contrato`),
+  ADD KEY `fk_ContratoServico` (`id_servico`),
+  ADD KEY `fk_ContratoCliente` (`id_cliente`),
+  ADD KEY `fk_ContratoPrestador` (`id_prestador`);
+
+--
+-- Índices para tabela `denuncia_comentario`
+--
+ALTER TABLE `denuncia_comentario`
+  ADD PRIMARY KEY (`id_denuncia_comentario`),
+  ADD KEY `fk_DenunciaComentario` (`id_comentario`);
+
+--
+-- Índices para tabela `denuncia_motivo`
+--
+ALTER TABLE `denuncia_motivo`
+  ADD PRIMARY KEY (`id_denuncia_motivo`);
+
+--
+-- Índices para tabela `denuncia_servico`
+--
+ALTER TABLE `denuncia_servico`
+  ADD PRIMARY KEY (`id_denuncia_servico`),
+  ADD KEY `fk_DenunciaServico` (`id_servico`),
+  ADD KEY `fk_DenunciaMotivo` (`motivo_denuncia`);
+
+--
+-- Índices para tabela `fale_conosco`
+--
+ALTER TABLE `fale_conosco`
+  ADD PRIMARY KEY (`id_contato`);
+
+--
+-- Índices para tabela `imagens`
+--
+ALTER TABLE `imagens`
+  ADD PRIMARY KEY (`id_imagem`),
+  ADD KEY `fk_ServicoImagem` (`id_servico`);
+
+--
+-- Índices para tabela `redes_sociais`
+--
+ALTER TABLE `redes_sociais`
+  ADD PRIMARY KEY (`id_rede_social`),
+  ADD KEY `fk_RedesocialUsuario` (`id_usuario`);
+
+--
+-- Índices para tabela `servico`
+--
+ALTER TABLE `servico`
+  ADD PRIMARY KEY (`id_servico`),
+  ADD KEY `fk_PrestadorServico` (`prestador`);
+
+--
+-- Índices para tabela `servicos_salvos`
+--
+ALTER TABLE `servicos_salvos`
+  ADD PRIMARY KEY (`id_servico_salvo`),
+  ADD KEY `fk_SalvoServico` (`id_servico`),
+  ADD KEY `fk_SalvoUsuario` (`id_usuario`);
 
 --
 -- Índices para tabela `usuarios`
@@ -74,10 +279,126 @@ ALTER TABLE `usuarios`
 --
 
 --
+-- AUTO_INCREMENT de tabela `comentarios`
+--
+ALTER TABLE `comentarios`
+  MODIFY `id_comentario` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `contratos`
+--
+ALTER TABLE `contratos`
+  MODIFY `id_contrato` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `denuncia_comentario`
+--
+ALTER TABLE `denuncia_comentario`
+  MODIFY `id_denuncia_comentario` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `denuncia_motivo`
+--
+ALTER TABLE `denuncia_motivo`
+  MODIFY `id_denuncia_motivo` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `denuncia_servico`
+--
+ALTER TABLE `denuncia_servico`
+  MODIFY `id_denuncia_servico` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `fale_conosco`
+--
+ALTER TABLE `fale_conosco`
+  MODIFY `id_contato` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `imagens`
+--
+ALTER TABLE `imagens`
+  MODIFY `id_imagem` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `redes_sociais`
+--
+ALTER TABLE `redes_sociais`
+  MODIFY `id_rede_social` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `servico`
+--
+ALTER TABLE `servico`
+  MODIFY `id_servico` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `servicos_salvos`
+--
+ALTER TABLE `servicos_salvos`
+  MODIFY `id_servico_salvo` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT de tabela `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restrições para despejos de tabelas
+--
+
+--
+-- Limitadores para a tabela `comentarios`
+--
+ALTER TABLE `comentarios`
+  ADD CONSTRAINT `fk_ComentarioServico` FOREIGN KEY (`id_servico`) REFERENCES `servico` (`id_servico`);
+
+--
+-- Limitadores para a tabela `contratos`
+--
+ALTER TABLE `contratos`
+  ADD CONSTRAINT `fk_ContratoCliente` FOREIGN KEY (`id_cliente`) REFERENCES `usuarios` (`id_usuario`),
+  ADD CONSTRAINT `fk_ContratoPrestador` FOREIGN KEY (`id_prestador`) REFERENCES `usuarios` (`id_usuario`),
+  ADD CONSTRAINT `fk_ContratoServico` FOREIGN KEY (`id_servico`) REFERENCES `servico` (`id_servico`);
+
+--
+-- Limitadores para a tabela `denuncia_comentario`
+--
+ALTER TABLE `denuncia_comentario`
+  ADD CONSTRAINT `fk_DenunciaComentario` FOREIGN KEY (`id_comentario`) REFERENCES `comentarios` (`id_comentario`);
+
+--
+-- Limitadores para a tabela `denuncia_servico`
+--
+ALTER TABLE `denuncia_servico`
+  ADD CONSTRAINT `fk_DenunciaMotivo` FOREIGN KEY (`motivo_denuncia`) REFERENCES `denuncia_motivo` (`id_denuncia_motivo`),
+  ADD CONSTRAINT `fk_DenunciaServico` FOREIGN KEY (`id_servico`) REFERENCES `servico` (`id_servico`);
+
+--
+-- Limitadores para a tabela `imagens`
+--
+ALTER TABLE `imagens`
+  ADD CONSTRAINT `fk_ServicoImagem` FOREIGN KEY (`id_servico`) REFERENCES `servico` (`id_servico`);
+
+--
+-- Limitadores para a tabela `redes_sociais`
+--
+ALTER TABLE `redes_sociais`
+  ADD CONSTRAINT `fk_RedesocialUsuario` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id_usuario`);
+
+--
+-- Limitadores para a tabela `servico`
+--
+ALTER TABLE `servico`
+  ADD CONSTRAINT `fk_PrestadorServico` FOREIGN KEY (`prestador`) REFERENCES `usuarios` (`id_usuario`);
+
+--
+-- Limitadores para a tabela `servicos_salvos`
+--
+ALTER TABLE `servicos_salvos`
+  ADD CONSTRAINT `fk_SalvoServico` FOREIGN KEY (`id_servico`) REFERENCES `servico` (`id_servico`),
+  ADD CONSTRAINT `fk_SalvoUsuario` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id_usuario`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
