bddefc8cafea4bd16803365fc78d70a1be9e544b
Gustavo Inacio <gutiinacio@gmai.com>
2021-10-31 19:27:12 -0300

    mobile bottom navigationBar included in all pages, except for fale conosco, que esta com um erro de coluna do bd

--- public/EncontrarProfissional/Listagem/listagem.php
+++ public/EncontrarProfissional/Listagem/listagem.php
@@ -9,16 +9,13 @@ if(isset($_GET['query'])){
 
 //caso haja cookies salvos no pc do usuário, ele vai logar com os cookies salvos
 require "../../../logic/entrar_cookie.php";
-// require "../../../logic/DbConnection.php";
-//     $connectClass = new DbConnection();
-//    $con = $connectClass->connect();
-//     $con->setAttribute(PDO::MYSQL_ATTR_INIT_COMMAND, "SET NAMES 'utf8mb4_general_ci'");
-    
-//     $query = "SELECT * from categorias";
-//     $stmt = $con->query($query);
-//     $a = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-//     print_r($a);
+$isProvider = false;
+
+if(isset($_SESSION['classificacao']) && $_SESSION['classificacao'] > 1){
+    $isProvider = true;
+}
+
 $logged = 'false';
 if (isset($_SESSION['idUsuario']) && $_SESSION['idUsuario'] !== ""){
     $logged = 'true';
@@ -504,6 +501,66 @@ if (isset($_SESSION['idUsuario']) && $_SESSION['idUsuario'] !== ""){
         </div>
     </footer>
 
+    
+<div id="mobileBottomNavbarSection-spacer"></div>
+    <section id="mobileBottomNavbarSection" class="d-fixed d-sm-none">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col mobile-navbar-item">
+                    <a href="../../Home/home.php">
+                        <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M25.333 29.3332H6.66634C5.92996 29.3332 5.33301 28.7363 5.33301 27.9999V15.2186C5.33301 14.865 5.47361 14.5259 5.72367 14.2759L15.057 4.94256C15.3071 4.69219 15.6465 4.55151 16.0003 4.55151C16.3542 4.55151 16.6936 4.69219 16.9437 4.94256L26.277 14.2759C26.5273 14.5256 26.6675 14.865 26.6663 15.2186V27.9999C26.6663 28.7363 26.0694 29.3332 25.333 29.3332ZM13.333 19.9999H18.6663V26.6666H23.9997V15.7706L15.9997 7.77056L7.99967 15.7706V26.6666H13.333V19.9999Z" fill="#888F98"/>
+                        </svg>
+
+                        <p>Home</p>
+                    </a>
+                </div>
+                <div class="col mobile-navbar-item ">
+                    <a href="../../Chat/chat.php">
+                        <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M10.7351 23.7713L11.6065 24.2371C12.9568 24.959 14.4648 25.3356 15.996 25.3333L16 25.3333C21.1548 25.3333 25.3333 21.1548 25.3333 16C25.3333 10.8452 21.1548 6.66667 16 6.66667C10.8452 6.66667 6.66668 10.8452 6.66668 16V16.004C6.66437 17.5352 7.04097 19.0432 7.76289 20.3935L8.22873 21.2649L7.51244 24.4876L10.7351 23.7713ZM4.00001 28L5.41121 21.6508C4.48185 19.9125 3.99705 17.9712 4.00001 16C4.00001 9.3724 9.37241 4 16 4C22.6276 4 28 9.3724 28 16C28 22.6276 22.6276 28 16 28C14.0288 28.003 12.0875 27.5181 10.3492 26.5888L4.00001 28Z" fill="#888F98"/>
+                        </svg>
+
+                        <p>Chat</p>
+                    </a>
+                </div>
+
+                <?php if($isProvider){ ?>
+                    <div class="col mobile-navbar-item getting-out">
+                        <a href="../../Perfil/CriacaoServico/criar_servico.php">
+
+                           <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M20.5714 9.85714H14.1429V3.42857C14.1429 2.63973 13.5031 2 12.7143 2H11.2857C10.4969 2 9.85714 2.63973 9.85714 3.42857V9.85714H3.42857C2.63973 9.85714 2 10.4969 2 11.2857V12.7143C2 13.5031 2.63973 14.1429 3.42857 14.1429H9.85714V20.5714C9.85714 21.3603 10.4969 22 11.2857 22H12.7143C13.5031 22 14.1429 21.3603 14.1429 20.5714V14.1429H20.5714C21.3603 14.1429 22 13.5031 22 12.7143V11.2857C22 10.4969 21.3603 9.85714 20.5714 9.85714Z" fill="white"/>
+                            </svg>
+                        </a>
+                    </div>
+                <?php } ?>
+
+                <div class="col mobile-navbar-item active">
+                    <a href="./listagem.php">
+                        <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M16.0005 29.3335C13.9845 29.3391 11.9941 28.883 10.1818 28.0001C9.51852 27.6775 8.88303 27.3006 8.28179 26.8734L8.09912 26.7401C6.44555 25.5196 5.09362 23.9364 4.14713 22.1121C3.16811 20.2239 2.66034 18.127 2.66706 16.0001C2.66706 8.63628 8.63666 2.66675 16.0005 2.66675C23.3643 2.66675 29.3339 8.63628 29.3339 16.0001C29.3405 18.1259 28.8332 20.2219 27.8551 22.1094C26.9099 23.9327 25.5599 25.5153 23.9085 26.7361C23.2855 27.1921 22.6244 27.5936 21.9325 27.9361L21.8258 27.9894C20.0124 28.877 18.0195 29.3368 16.0005 29.3335ZM16.0005 22.6667C14.0024 22.6628 12.1707 23.7788 11.2578 25.5561C14.2463 27.0363 17.7546 27.0363 20.7431 25.5561V25.5494C19.8291 23.7741 17.9973 22.6606 16.0005 22.6667ZM16.0005 20.0001C18.8886 20.0039 21.5517 21.5602 22.9725 24.0747L22.9925 24.0574L23.0111 24.0414L22.9885 24.0614L22.9751 24.0721C26.3471 21.1589 27.5528 16.4565 25.9987 12.2802C24.4445 8.10392 20.4579 5.33384 16.0018 5.33384C11.5457 5.33384 7.55912 8.10392 6.00493 12.2802C4.45074 16.4565 5.65646 21.1589 9.02846 24.0721C10.4501 21.5588 13.1129 20.0036 16.0005 20.0001ZM16.0005 18.6667C13.0549 18.6667 10.6671 16.2789 10.6671 13.3334C10.6671 10.3879 13.0549 8.00008 16.0005 8.00008C18.946 8.00008 21.3338 10.3879 21.3338 13.3334C21.3338 14.7479 20.7719 16.1045 19.7717 17.1047C18.7715 18.1048 17.4149 18.6667 16.0005 18.6667ZM16.0005 10.6667C14.5277 10.6667 13.3338 11.8607 13.3338 13.3334C13.3338 14.8062 14.5277 16.0001 16.0005 16.0001C17.4732 16.0001 18.6671 14.8062 18.6671 13.3334C18.6671 11.8607 17.4732 10.6667 16.0005 10.6667Z" fill="#888F98"/>
+                        </svg>
+
+
+                        <p>Pesquisar</p>
+                    </a>
+                </div>
+                <div class="col mobile-navbar-item ">
+                    <a href="../../Perfil/meu_perfil.php">
+                        <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M9.33301 10.6667C9.33301 6.98477 12.3178 4 15.9997 4C19.6816 4 22.6663 6.98477 22.6663 10.6667C22.6663 14.3486 19.6816 17.3333 15.9997 17.3333C12.3178 17.3333 9.33301 14.3486 9.33301 10.6667ZM15.9997 14.6667C18.2088 14.6667 19.9997 12.8758 19.9997 10.6667C19.9997 8.45753 18.2088 6.66667 15.9997 6.66667C13.7905 6.66667 11.9997 8.45753 11.9997 10.6667C11.9997 12.8758 13.7905 14.6667 15.9997 14.6667Z" fill="#888F98"/>
+                            <path d="M8.4572 21.7909C6.45681 23.7912 5.33301 26.5044 5.33301 29.3333H7.99967C7.99967 27.2116 8.84253 25.1768 10.3428 23.6765C11.8431 22.1762 13.8779 21.3333 15.9997 21.3333C18.1214 21.3333 20.1562 22.1762 21.6565 23.6765C23.1568 25.1768 23.9997 27.2116 23.9997 29.3333H26.6663C26.6663 26.5044 25.5425 23.7912 23.5421 21.7909C21.5418 19.7905 18.8286 18.6667 15.9997 18.6667C13.1707 18.6667 10.4576 19.7905 8.4572 21.7909Z" fill="#888F98"/>
+                        </svg>
+
+
+                        <p>Perfil</p>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </section>
+
     <script type="module">
         import {setSearchState, setTempPosition, userNotLogged, tempPosition} from './listagem.js';
 
@@ -559,115 +616,9 @@ if (isset($_SESSION['idUsuario']) && $_SESSION['idUsuario'] !== ""){
             }
         ?>
         userNotLogged('<?=$is_set_location?>')
-
-
-    // let btnSearch = document.querySelector("#searchButton");
-
-        // const handleBtnSearch = (event) => {
-        //     event.preventDefault();
-        //     let text = document.querySelector("#searchBar").value;
-        //     text = text.trim();
-
-        //     let spliText = text.split(" ")
-        //     if(text == "") spliText = [];   
-
-        //     let serviceCardsPath = document.querySelector(".service-cards-path");
-        //     serviceCardsPath.innerHTML = "";
-
-        //     setSearchState({write : spliText});
-            
-        // };
-
-        <?php if(!$searchQuery == ""){?>
-            // let btnSearch = document.querySelector("#searchButton");
-            // btnSearch.onclick = handleBtnSearch;
-        <?php }?>
-
     </script>
 
-    <!-- <div id="cat-container" class="container-fluid ">
-        <div class="row">
-            <section class="cool-categories-section col-3">
-                <header class="row">
-                    <h1 class="h1">Categorias</h1>
-                </header>
-                <div class="content row">
-                    
-                    <div class="col-12 cat-view">
-                         <div class="row cat-item selected">
-                            <div class="col">
-                                <p class="cat-text">Programação e Tecnologia</p>
 
-                                <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <path d="M7.482 11.831L1.54814 5.89714C1.26195 5.61096 1.26195 5.14698 1.54814 4.86082L2.24023 4.16873C2.52592 3.88304 2.98896 3.88249 3.27532 4.16751L8.00018 8.87023L12.725 4.16751C13.0114 3.88249 13.4744 3.88304 13.7601 4.16873L14.4522 4.86082C14.7384 5.14701 14.7384 5.61099 14.4522 5.89714L8.51835 11.831C8.23217 12.1172 7.76819 12.1172 7.482 11.831Z" fill="#3333CC"/>
-                                </svg>
-                            </div>
-                        </div>
-                        <div class="row cat-item">
-                            <div class="col">
-                                <p class='cat-text'>Beleza</p>
 
-                                <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <path d="M7.482 11.831L1.54814 5.89714C1.26195 5.61096 1.26195 5.14698 1.54814 4.86082L2.24023 4.16873C2.52592 3.88304 2.98896 3.88249 3.27532 4.16751L8.00018 8.87023L12.725 4.16751C13.0114 3.88249 13.4744 3.88304 13.7601 4.16873L14.4522 4.86082C14.7384 5.14701 14.7384 5.61099 14.4522 5.89714L8.51835 11.831C8.23217 12.1172 7.76819 12.1172 7.482 11.831Z" fill="#3333CC"/>
-                                </svg>
-                            </div>
-                        </div>
-                        <div class="row cat-item">
-                            <div class="col">
-                                <p class="cat-text">Programação e Tecnologia</p>
-
-                                <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <path d="M7.482 11.831L1.54814 5.89714C1.26195 5.61096 1.26195 5.14698 1.54814 4.86082L2.24023 4.16873C2.52592 3.88304 2.98896 3.88249 3.27532 4.16751L8.00018 8.87023L12.725 4.16751C13.0114 3.88249 13.4744 3.88304 13.7601 4.16873L14.4522 4.86082C14.7384 5.14701 14.7384 5.61099 14.4522 5.89714L8.51835 11.831C8.23217 12.1172 7.76819 12.1172 7.482 11.831Z" fill="#3333CC"/>
-                                </svg>
-                            </div>
-                        </div>
-                        <div class="row cat-item">
-                            <div class="col">
-                                <p class='cat-text'>Beleza</p>
-
-                                <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <path d="M7.482 11.831L1.54814 5.89714C1.26195 5.61096 1.26195 5.14698 1.54814 4.86082L2.24023 4.16873C2.52592 3.88304 2.98896 3.88249 3.27532 4.16751L8.00018 8.87023L12.725 4.16751C13.0114 3.88249 13.4744 3.88304 13.7601 4.16873L14.4522 4.86082C14.7384 5.14701 14.7384 5.61099 14.4522 5.89714L8.51835 11.831C8.23217 12.1172 7.76819 12.1172 7.482 11.831Z" fill="#3333CC"/>
-                                </svg>
-                            </div>
-                        </div>
-                        <div class="row cat-item">
-                            <div class="col">
-                                <p class="cat-text">Programação e Tecnologia</p>
-
-                                <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <path d="M7.482 11.831L1.54814 5.89714C1.26195 5.61096 1.26195 5.14698 1.54814 4.86082L2.24023 4.16873C2.52592 3.88304 2.98896 3.88249 3.27532 4.16751L8.00018 8.87023L12.725 4.16751C13.0114 3.88249 13.4744 3.88304 13.7601 4.16873L14.4522 4.86082C14.7384 5.14701 14.7384 5.61099 14.4522 5.89714L8.51835 11.831C8.23217 12.1172 7.76819 12.1172 7.482 11.831Z" fill="#3333CC"/>
-                                </svg>
-                            </div>
-                        </div>
-                        <div class="row cat-item">
-                            <div class="col">
-                                <p class='cat-text'>Beleza</p>
-
-                                <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <path d="M7.482 11.831L1.54814 5.89714C1.26195 5.61096 1.26195 5.14698 1.54814 4.86082L2.24023 4.16873C2.52592 3.88304 2.98896 3.88249 3.27532 4.16751L8.00018 8.87023L12.725 4.16751C13.0114 3.88249 13.4744 3.88304 13.7601 4.16873L14.4522 4.86082C14.7384 5.14701 14.7384 5.61099 14.4522 5.89714L8.51835 11.831C8.23217 12.1172 7.76819 12.1172 7.482 11.831Z" fill="#3333CC"/>
-                                </svg>
-                            </div>
-                        </div> -->
-                    </div>
-                    <!-- <div class="col-12 subcat-view">
-                        <header class="row">
-                            <div class="back-to-main-menu">
-                                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <path d="M7.9848 13.7857H21.4643C21.7601 13.7857 22 13.5458 22 13.25V10.75C22 10.4541 21.7601 10.2143 21.4643 10.2143H7.9848V8.15806C7.9848 7.20351 6.83074 6.72547 6.15574 7.40043L2.31383 11.2423C1.89539 11.6608 1.89539 12.3392 2.31383 12.7576L6.15574 16.5995C6.83069 17.2744 7.9848 16.7964 7.9848 15.8418V13.7857Z" fill="#3333CC"/>
-                                </svg>
-
-                                <h1>Voltar Para o menu Principal</h1>
-                            </div>
-                        </header>
-                        <div class="row content">
-                            
-                        </div>
-                    </div>
-
-                </div>
-                
-            </section>
-        </div>
-    </div> -->
 </body>
 </html>
